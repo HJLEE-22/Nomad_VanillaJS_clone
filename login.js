4db068da26bda33fb30fc05ebe084542920cea12
HJLEE-22 <hyungjuice@naver.com>
2022-03-12 15:39:20 +0900

    Using LocalStorage
    my version of Using localStorage for greeting to user who logged in berfore.
    * diffrence with Nico : He added another function to shorten the innertext element.

--- login.js
+++ login.js
@@ -2,16 +2,34 @@ const loginForm = document.querySelector("#login_form")
 const loginInput = document.querySelector("#login_input")
 const loginButton = document.querySelector("#login_button")
 const greeting = document.querySelector("#greeting")
+const USERNAME_KEY = "username"
+
 
 function handleLogin(event){
     event.preventDefault();
     loginForm.classList.add("hidden");
 
-    const username = loginInput.value
+    const username = loginInput.value;
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove("hidden");
-
+    localStorage.setItem(USERNAME_KEY, username)
 }
 
-loginForm.addEventListener("submit", handleLogin)
-//form에 이벤트리스너를 걸어야 하는군//
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function greetingToUser(){
+    greeting.classList.remove("hidden");
+    greeting.innerText = `Hello ${savedUsername}`;
+}
+
+
+if(savedUsername===null){
+    loginForm.classList.remove("hidden");
+    loginForm.addEventListener("submit", handleLogin);
+} else{
+    greetingToUser();
+}
+
+
+
+
